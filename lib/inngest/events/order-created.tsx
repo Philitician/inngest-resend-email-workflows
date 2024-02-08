8c9cd17ccbbdfa3ce1362de36c0f15f0141adf3b
Philip Rothschild <philiprene89@gmail.com>
2024-02-08 17:54:39 +0100

    include order link in order confirmation email

--- lib/inngest/events/order-created.tsx
+++ lib/inngest/events/order-created.tsx
@@ -12,20 +12,26 @@ export const orderCreated = inngest.createFunction(
     const { orderId } = event.data;
     const { userId, email } = event.user;
     await step.run("Send confirmation email", async () => {
-      const heads = headers();
-      console.log("heads", heads);
-      console.log("VERCEL_URL: ", env.VERCEL_URL || "http://localhost:3000");
-      // const { data } = await resend.emails.send({
-      //   from: "Testing Resend <onboarding@resend.dev>",
-      //   to: email,
-      //   subject: "Hello World!!",
-      //   react: (
-      //     <BasicEmail>
-      //       <Button href={`/order/${orderId}`}>View your order</Button>
-      //     </BasicEmail>
-      //   ),
-      // });
-      // console.log("Email sent", data);
+      const { data } = await resend.emails.send({
+        from: "Ordrespesialisten <onboarding@resend.dev>",
+        to: email,
+        subject: "Ordre opprettet!",
+        react: (
+          <BasicEmail>
+            <Button href={getOrderUrl(orderId)}>
+              Sjekk ut ordre {orderId}
+            </Button>
+          </BasicEmail>
+        ),
+      });
+      console.log("Email sent", data);
     });
   }
 );
+
+const getOrderUrl = (orderId: number) => {
+  const heads = headers();
+  const host = heads.get("x-forwarded-host");
+  const protocol = heads.get("x-forwarded-proto");
+  return `${protocol}://${host}/order/${orderId}`;
+};
